15ae6f8179fc92c7798102c6139aa8000c8fdc81
tHyt-lab <11361677+tHyt-lab@users.noreply.github.com>
2024-12-26 22:31:49 +0900

    add image viewer and button components to enhance home screen layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+import { StyleSheet, View } from "react-native";
+
+const PlaceholderImage = require('@/assets/images/background-image.png')
 
 export default function Index() {
     return (
-        <View
-            style={styles.container}
-        >
-            <Text style={styles.text}>Home screen</Text>
-            <Link href="/about" style={styles.button}>
-                Go to about
-            </Link>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer imgSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button theme="primary" label="Choose a photo" />
+                <Button label="Use this photo" />
+            </View>
         </View>
     );
 }
@@ -17,16 +21,15 @@ export default function Index() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: '#25292e'
+        backgroundColor: '#25292e',
+        alignItems: 'center',
     },
-    text: {
-        color: '#fff',
+    imageContainer: {
+        flex: 1,
+        paddingTop: 28,
     },
-    button: {
-        fontSize: 20,
-        color: '#fff',
-        textDecorationLine: 'underline',
-    }
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
 })
