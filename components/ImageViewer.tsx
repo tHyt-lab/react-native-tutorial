91dd46f9275ad040ec09f14e2a3a04d79781a345
tHyt-lab <11361677+tHyt-lab@users.noreply.github.com>
2024-12-26 22:49:37 +0900

    Added image selection feature and updated button component to allow image selection

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,11 +3,14 @@ import { StyleSheet } from "react-native";
 
 type Props = {
     imgSource: ImageSource
+    selectedImage?: string
 }
 
-export default function ImageViewer({ imgSource }: Props) {
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+    const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
     return (
-        <Image source={imgSource} style={styles.image} />
+        <Image source={imageSource} style={styles.image} />
     );
 }
 
